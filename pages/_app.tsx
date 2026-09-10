0f3e75e5156a6fc08f7b785d43a75a7ee52ad6b6
ulugbek-lab <bekievbek02@gmail.com>
2026-09-10 22:51:28 +0900

    feat: meta tags integration on document private file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,6 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+  console.log("App COMPONENT - PAGES ROUTER");
   return <Component {...pageProps} />;
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,26 @@ export default function Document() {
   console.log("DOCUMENT - PAGES ROUTER");
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index, follow" />
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+        
+        {/** SEO */}
+        <meta
+          name="keyword"
+          content={"nestar, nestar.uz, devex mern, mern nestjs fullstack"}
+        />
+        <meta
+          name="description"
+          content={
+            "Buy and sell properties anytime in South Korea. | " +
+            "Покупайте и продавайте недвижимость в Южной Корее в любое время. | " +
+            "대한민국에서 언제든지 부동산을 사고파세요."
+          }
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
