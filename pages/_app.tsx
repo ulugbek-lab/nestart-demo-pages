f841b044c50d41384bc67cb7a3472c8f80a74757
ulugbek-lab <bekievbek02@gmail.com>
2026-09-10 23:17:57 +0900

    feat: Mui customised integartion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { light } from "../scss/MaterialTheme";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  console.log("App COMPONENT - PAGES ROUTER");
-  return <Component {...pageProps} />;
+  //@ts-ignore
+  const [theme, setTheme] = useState(createTheme(light));
+
+  //socket.io, Redux, Mui
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
