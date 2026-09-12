7f8b0c2ddc52d82f6e588c90de804cfd11b851b8
ulugbek-lab <bekievbek02@gmail.com>
2026-09-12 23:07:09 +0900

    feat: SASS integartion

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,19 @@ import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <>
-      <Container>
-        <Stack flexDirection={"column"}>
-          <Box>Popular Properties</Box>
-          <Box>Top Agents</Box>
-          <Box>Top Properties</Box>
-          <Box>Events</Box>
+    <Stack>
+      <Stack flexDirection={"column"}>
+        <Stack>
+          <Stack className="container">Popular Properties</Stack>
         </Stack>
-      </Container>
-    </>
+        <Stack>
+          <Stack className="container">Top Agents</Stack>
+        </Stack>
+        <Stack>
+          <Stack className="container">Top Properties</Stack>
+        </Stack>
+      </Stack>
+    </Stack>
   );
 };
 export default withLayoutMain(Home);
