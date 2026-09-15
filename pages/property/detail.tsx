cffe80dc53cd26d17b58fe60e0de0e4eae37a40a
ulugbek-lab <bekievbek02@gmail.com>
2026-09-15 17:32:37 +0900

    feat: publish property list component

--- pages/property/detail.tsx
+++ pages/property/detail.tsx
@@ -1,9 +1,15 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
-import { Container } from "@mui/material";
-import { NextPage } from "next";
+import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
+import { Container, Stack } from "@mui/material";
 
-const PropertyDetail: NextPage = () => {
-  return <Container> PROPERTY DETAIL</Container>;
+const PropertyDetail = () => {
+  const device = useDeviceDetect();
+
+  if (device === "mobile") {
+    return <Stack>PROPERTY DETAIL MOBILE</Stack>;
+  } else {
+    return <Container>PROPERTY DETAIL</Container>;
+  }
 };
 
 export default withLayoutBasic(PropertyDetail);
